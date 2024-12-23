deff0929749a1fc613ca5128d1b1b7d30a92a028
Polar-Lynx <ozaoui7150@conestogac.on.ca>
2024-12-23 16:38:53 -0500

    Added and implemented react-router package

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+// utilities
+import { BrowserRouter, Route, Routes } from "react-router";
+
+
+// components
 import HomePageView from './components/HomePage/HomePageView';
 
 
 const App = () => {
     return (
-        <div className="App">
-            <HomePageView />
-        </div>
+        <BrowserRouter>
+            {/* couple URL segments to UI elements */}
+            <Routes>
+                {/* couples a URL segment to a component */}
+                <Route path="/" element={<HomePageView />} />
+            </Routes>
+        </BrowserRouter>
     );
 };
 
