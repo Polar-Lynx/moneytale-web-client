8a0acd3fc157136668d6b826a439b71b1fe5f02c
Polar-Lynx <ozaoui7150@conestogac.on.ca>
2024-12-24 01:40:49 -0500

    Added model component for account creation

---
+++ src/components/AccountCreation/AccountCreationModel.jsx
@@ -0,0 +1,16 @@
+/********************************************************************************
+*                       MODELS                                                  *
+********************************************************************************/
+// stores user creation form data
+export const initialFormData = {
+    name: "",
+    email: "",
+    password: ""
+};
+
+// tracks validation errors
+export const initialInputErrors = {
+    name: false,
+    email: false,
+    password: false
+};
