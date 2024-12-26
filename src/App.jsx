7c428ce96ebaca0531c533687488c4e774f8d37e
Polar-Lynx <ozaoui7150@conestogac.on.ca>
2024-12-26 04:53:38 -0500

    Added DashboardView to app's routes, with sample data passed as props

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import { BrowserRouter, Route, Routes } from "react-router";
 ********************************************************************************/
 import { HomePageView } from './components/HomePage/HomePageView';
 import { AccountSelectionView } from "./components/AccountSelection/AccountSelectionView";
+import { DashboardView } from "./components/Dashboard/DashboardView";
 
 
 export const App = () => {
@@ -20,6 +21,15 @@ export const App = () => {
                 <Route path="/" element={<HomePageView />} />
 
                 <Route path="/account-selection" element={<AccountSelectionView />} />
+
+                <Route path="/dashboard" element={
+                    <DashboardView
+                        financialGoals={ [{ id: "1", title: "Cut Costs", description: "Spend less money eating outside!"}, { id: "2", title: "Save Money", description: "Use Coupon apps."}, { id: "3", title: "Pay Back Loans", description: "Use any leftover money to pay back borrowed money."}] }
+                        latestIncomeRecords={ [{ id: "1", description: "7-Eleven", amount: "52.45", date: "2024-12-23"}, { id: "2", description: "Walmart", amount: "134.97", date: "2024-12-30"}] }
+                        latestSpendingRecords={ [{ id: "1", description: "7-Eleven", amount: "52.45", date: "2024-12-23"}, { id: "2", description: "Walmart", amount: "134.97", date: "2024-12-30"}, { id: "3", description: "Planet Fitness", amount: "32.47", date: "2025-1-1"}] }
+                        userName={ "Mario" }
+                    />
+                } />
             </Routes>
         </BrowserRouter>
     );
