857a7a3b1808c0efa4f4818cf752d3a46307f0fb
Polar-Lynx <ozaoui7150@conestogac.on.ca>
2024-12-26 04:49:35 -0500

    Added model component for AccountSelection

---
+++ src/components/AccountSelection/AccountSelectionModel.jsx
@@ -0,0 +1,14 @@
+/********************************************************************************
+*                       MODELS                                                  *
+********************************************************************************/
+// stores user identification form data
+export const initialFormData = {
+    email: "",
+    password: ""
+};
+
+// tracks validation errors
+export const initialInputErrors = {
+    email: false,
+    password: false
+};
