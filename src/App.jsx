02f5f91ae2de2921c5cb864be334aeb5c956c020
Polar-Lynx <ozaoui7150@conestogac.on.ca>
2025-01-05 06:51:54 -0500

    Wrapped Dashboard and AccountSelection components with Dashboard context

--- src/App.jsx
+++ src/App.jsx
@@ -9,26 +9,26 @@ import { BrowserRouter, Route, Routes } from "react-router";
 ********************************************************************************/
 import { HomePageView } from './components/HomePage/HomePageView';
 import { AccountSelectionView } from "./components/AccountSelection/AccountSelectionView";
+import { DashboardContextProvider } from "./components/Dashboard/DashboardContext";
 import { DashboardView } from "./components/Dashboard/DashboardView";
 
 
 export const App = () => {
     return (
         <BrowserRouter>
-            {/* couples URL segments to UI elements */}
             <Routes>
-                {/* couples a URL segment to a component */}
                 <Route path="/" element={<HomePageView />} />
 
-                <Route path="/account-selection" element={<AccountSelectionView />} />
+                <Route path="/account-selection" element={
+                    <DashboardContextProvider>
+                        <AccountSelectionView />
+                    </DashboardContextProvider>
+                } />
 
                 <Route path="/dashboard" element={
-                    <DashboardView
-                        financialGoals={ [{ id: "1", title: "Cut Costs", description: "Spend less money eating outside!"}, { id: "2", title: "Save Money", description: "Use Coupon apps."}, { id: "3", title: "Pay Back Loans", description: "Use any leftover money to pay back borrowed money."}] }
-                        latestIncomeRecords={ [{ id: "1", description: "7-Eleven", amount: "52.45", date: "2024-12-23"}, { id: "2", description: "Walmart", amount: "134.97", date: "2024-12-30"}] }
-                        latestSpendingRecords={ [{ id: "1", description: "7-Eleven", amount: "52.45", date: "2024-12-23"}, { id: "2", description: "Walmart", amount: "134.97", date: "2024-12-30"}, { id: "3", description: "Planet Fitness", amount: "32.47", date: "2025-1-1"}] }
-                        userName={ "Mario" }
-                    />
+                    <DashboardContextProvider>
+                        <DashboardView />
+                    </DashboardContextProvider>
                 } />
             </Routes>
         </BrowserRouter>
